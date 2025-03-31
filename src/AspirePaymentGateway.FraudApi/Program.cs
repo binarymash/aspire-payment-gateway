98f8c8ae8d26916b479794e9058e48b3cf929713
Philip Wood <philip.m.wood@gmail.com>
2025-03-31 10:29:29 +0100

    Fix fraud api service registration

--- src/AspirePaymentGateway.FraudApi/Program.cs
+++ src/AspirePaymentGateway.FraudApi/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 
 // Add services to the container.
+builder.Services.AddSingleton<ScreeningHandler>();
 
 var app = builder.Build();
 
